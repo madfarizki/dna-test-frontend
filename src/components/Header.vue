069e24ad0e47dc8cd2fee3a2c22bb1419694a857
Ahmad Alfarizki <ahmfariz24@gmail.com>
2024-03-06 22:24:48 +0700

    feat: initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="container">
+    <div class="logo">
+      <h2>MADNews</h2>
+    </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
