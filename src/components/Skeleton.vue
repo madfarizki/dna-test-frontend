956665d3dd0cccd2746493322742323991504aec
Ahmad Alfarizki <ahmfariz24@gmail.com>
2024-03-07 13:27:38 +0700

    feat: skeleton animation

--- src/components/Skeleton.vue
+++ src/components/Skeleton.vue
@@ -9,9 +9,10 @@
 <style lang="scss" scoped>
 .skeleton {
   aspect-ratio: 1;
-  background: #e7e7e7;
-  display: flex;
+  background: linear-gradient(90deg, rgba(231, 231, 231, 1) 0%, rgb(218, 218, 218) 50%, rgb(243, 243, 243) 100%);
+  background-size: 200% 100%;
   border-radius: 5px;
+  animation: shine 2s infinite linear;
 
   @media screen and (max-width: 800px) {
     grid-column: 1;
@@ -43,6 +44,18 @@
 
     &:nth-child(10n + 9) {
       grid-column: 2;
+    }
+  }
+
+  @keyframes shine {
+    0% {
+      background-position: 200% 0;
+      /* Start with the gradient fully shifted */
+    }
+
+    100% {
+      background-position: -200% 0;
+      /* Shift the gradient fully to the left */
     }
   }
 }
